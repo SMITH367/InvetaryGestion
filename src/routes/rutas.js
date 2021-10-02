9d751d976335b515518ff80f07728c26a4672442
Brayan Puentes <brayansmithpuente@gmail.com>
2021-10-01 20:57:15 -0500

    added get / get byid to the api

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -4,4 +4,24 @@ const router = express.Router()
 const conexionmysql = require('../database')
 
 
+router.get('/products', (req, res) => {
+    conexionmysql.query('SELECT * FROM productos', (err,rows,fields) => {
+        if (!err) {
+            res.status(200).send(rows);
+        } else {
+            console.log(err)
+        }
+    })
+})
+router.get('/:id', (req, res) => {
+    let id = req.params.id
+    conexionmysql.query('SELECT * FROM productos WHERE codigo = ?', [id], (err,rows,fields) => {
+        if (!err) {
+            res.json(rows)
+        } else {
+            console.log(err)
+        }
+    })
+    
+})
 module.exports = router 

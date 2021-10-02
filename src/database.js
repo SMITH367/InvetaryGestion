855370e0992c619ee0a2d7c25e4a017d8483106f
Brayan Puentes <brayansmithpuente@gmail.com>
2021-10-01 20:55:05 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql')
+
+const conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database:"productos"
+})
+conexion.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("conectado")
+    }
+})
+module.exports = conexion
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+app.set('port', 3000)
+app.set('address','localhost')
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use(express.json())
+
+app.use(require('./routes/rutas'))
+
+
+app.listen(app.get('port'),()=>{
+    console.log('server en el puerto 3k')
+})
+

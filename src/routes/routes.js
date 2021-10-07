343362b914feea65b6e7af4e63861a8e3969613a
Brayan Puentes <brayansmithpuente@gmail.com>
2021-10-07 00:26:01 -0500

    change in structure

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const conexionmysql = require('../database')
+const conexionMysql = require('../database')
 
 
 router.get('/products', (req, res) => {
-    conexionmysql.query('SELECT * FROM productos', (err,rows,fields) => {
+    conexionMysql.query('SELECT * FROM productos', (err,rows,fields) => {
         if (!err) {
             res.status(200).send(rows);
         } else {
@@ -15,7 +15,7 @@ router.get('/products', (req, res) => {
 })
 router.get('/products/:id', (req, res) => {
     let id = req.params.id
-    conexionmysql.query('SELECT * FROM productos WHERE codigo = ?', [id], (err,rows,fields) => {
+    conexionMysql.query('SELECT * FROM productos WHERE codigo = ?', [id], (err,rows,fields) => {
         if (!err) {
             res.json(rows)
         } else {
@@ -27,7 +27,7 @@ router.get('/products/:id', (req, res) => {
 router.post('/products', (req, res) => {
 
     let data = req.body
-    conexionmysql.query('INSERT INTO productos SET ?', [data], (err, rows, fields) => {
+    conexionMysql.query('INSERT INTO productos SET ?', [data], (err, rows, fields) => {
         if (err) res.send(err)
         else {
             res.json("Product added");
@@ -40,7 +40,7 @@ router.put('/products/:id', (req, res) => {
     let id = req.params.id
     console.log(data)
     console.log(id)
-    conexionmysql.query('UPDATE productos SET ? WHERE codigo = ? ', [data, id], (err, rows, fields) => {
+    conexionMysql.query('UPDATE productos SET ? WHERE codigo = ? ', [data, id], (err, rows, fields) => {
         if (err) res.send(err);
         else {
             res.json("Update complete")
@@ -51,7 +51,7 @@ router.delete('/products/:id', (req, res) => {
 
     let data = req.params.id
     console.log(data)
-    conexionmysql.query('DELETE FROM productos WHERE codigo = ?', [data], (err, rows, fields) => {
+    conexionMysql.query('DELETE FROM productos WHERE codigo = ?', [data], (err, rows, fields) => {
         if (err) res.send(err)
         else {
             res.json("Product deleted");

9c32464718bb392cd15b5fd19fc3be235fd4fa22
Brayan Puentes <brayansmithpuente@gmail.com>
2021-11-20 18:08:00 -0500

    tested

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password:"Smith367@1234",
+    password:"1234",
     database:"productos"
 })
 conexion.connect((err) => {
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 
 app.set('port', 3000)
-app.set('address', '129.151.123.58')
-
+app.set('address','localhost')
 
 //Midelwares
 app.use((req, res, next) => {
